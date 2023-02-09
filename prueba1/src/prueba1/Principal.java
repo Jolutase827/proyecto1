36e111e945afd17f9b00174f363a17fc737f95c9
Jose Luis <jolutase104@gmail.com>
2023-02-09 12:05:36 +0100

    Segundo Commit

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -2,6 +2,6 @@ package prueba1;
 
 public class Principal {
 	public static void main (String[] args) {
-		System.out.println("Hola Mundo! java-git");
+		System.out.println("Hola Mundo! segundo");
 	}
 }
