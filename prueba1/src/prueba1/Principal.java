4bf2d0c89468eefa23545db702e5b76294d5b114
Jose Luis <jolutase104@gmail.com>
2023-02-09 12:20:27 +0100

    Cambio en principal con cambio1

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -2,6 +2,10 @@ package prueba1;
 
 public class Principal {
 	public static void main (String[] args) {
-		System.out.println("Hola Mundo! otro mas");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo2 java-git");
+		System.out.println("Hola mundo3 java-git");
+		System.out.println("Pruebas Git. Se añade Cambio 1");
+
 	}
 }
